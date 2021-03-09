e8395cff3e6968b0e1c617c0896d257df19daa3c
Alon Reik <alon.reik@gmail.com>
2021-03-09 17:13:18 +0200

    Committed the changes made in lecture #2 .

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Alon Reik on 09/03/2021.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return Card.identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+    
+}
