adedb01afebcd7439bbf1edc4d0bd3f98f0b59f2
Alon Reik <alon.reik@gmail.com>
2021-03-11 16:59:06 +0200

    Updated all usages of the themes dictionaries to comply with the new unified dictionary.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -53,8 +53,8 @@ class ViewController: UIViewController {
     
     // Resets all Model and View properties
     func startNewGame() {
-        emojiChoices = themesEmojies.randomElement()! // choose a new random theme
-        background.backgroundColor = themeColors[emojiChoices.key]!.1
+        emojiChoices = themesDict.randomElement()! // choose a new random theme
+        background.backgroundColor = themesDict[emojiChoices.key]!.2
         emoji.removeAll() // clear previous mapping between cards and emojis
         game.resetGame()
         
@@ -72,7 +72,7 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : themeColors[emojiChoices.key]!.0
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : themesDict[emojiChoices.key]!.1
             }
         }
     }
@@ -89,36 +89,16 @@ class ViewController: UIViewController {
                       "Flags": (["🏳️‍🌈", "🏳️", "🏴","🏴‍☠️","🏁","🇧🇿","🇫🇯","🇹🇭","🇮🇳"], #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
                     ]
     
-    
-    // A dictionary mapping themes (Strings) to an array of emoji choices (array of Strings)
-    let themesEmojies = ["Halloween": ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"],
-                  "Animals": ["🙀", "🦇", "🐱", "🐥", "🐦", "🦉", "🐺", "🦄", "🐷" ],
-                  "Sports": ["⚽️", "🥎", "⚾️", "🥋", "🎽", "🥏", "🤿","🏒", "🏉" ],
-                  "Food": ["🍭", "🎃", "🍎", "🍬", "🍌", "🥝" ,"🍞", "🥕", "🥯"],
-                  "Faces": ["😱", "😀", "😋", "🥸", "😜", "🥳", "😛", "🤩", "😏"],
-                  "Flags": ["🏳️‍🌈", "🏳️", "🏴","🏴‍☠️","🏁","🇧🇿","🇫🇯","🇹🇭","🇮🇳"]
-                ]
-    
-    // A dictionary mapping themes (Strings) to tuples of color literals, where the first
-    // color is for the background (bg) of the cards, and the second is for the overall bg.
-    let themeColors = ["Halloween": (#colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)),
-                       "Animals": (#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)),
-                       "Sports": (#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)),
-                       "Food": (#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)),
-                       "Faces": (#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)),
-                       "Flags": (#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
-                     ]
-    
-    lazy var emojiChoices = themesEmojies.randomElement()!
+    lazy var emojiChoices = themesDict.randomElement()!
 
     // A dictionary mapping an identifier of a card (Int) to an emoji (String)
     var emoji = [Int: String]()
     
     // Assigns the given card with an emoji from the emojiChoices array.
     func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.value.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.value.count)))
-            emoji[card.identifier] = emojiChoices.value.remove(at: randomIndex)
+        if emoji[card.identifier] == nil, emojiChoices.value.0.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.value.0.count)))
+            emoji[card.identifier] = emojiChoices.value.0.remove(at: randomIndex)
         }
         return emoji[card.identifier] ?? "?"
     }
